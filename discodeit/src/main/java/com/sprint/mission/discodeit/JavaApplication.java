9c818bfd91082129ad9738db6b8411c47bb8fbad
DonToong2 <rlaaudrms369@gmail.com>
2026-02-25 15:24:28 +0900

    테스트 종료

--- discodeit/src/main/java/com/sprint/mission/discodeit/JavaApplication.java
+++ discodeit/src/main/java/com/sprint/mission/discodeit/JavaApplication.java
@@ -17,7 +17,6 @@ import java.util.UUID;
 
 public class JavaApplication {
     public static void main(String[] args) {
-        System.out.println("git user.name, user.email 일치 테스트.");
         UserService userService = new FileUserService();
         ChannelService channelService = new JCFChannelService();
         MessageService messageService = new JCFMessageService();
