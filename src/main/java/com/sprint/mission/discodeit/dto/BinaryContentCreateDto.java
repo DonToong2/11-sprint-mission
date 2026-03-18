e194986ba13a9d45831e456e8db805942e10aedc
김명근 <rlaaudrms369@gmail.com>
2026-03-18 16:29:54 +0900

    기존 BinaryContentCreateDto에 userId와messageId 추가 후 BinaryContentService 인터페이스와 구현체 생성

--- src/main/java/com/sprint/mission/discodeit/dto/BinaryContentCreateDto.java
+++ src/main/java/com/sprint/mission/discodeit/dto/BinaryContentCreateDto.java
@@ -1,6 +1,10 @@
 package com.sprint.mission.discodeit.dto;
 
+import java.util.UUID;
+
 public record BinaryContentCreateDto(
+        UUID userId,
+        UUID messageId,
         byte[] bytes,
         String fileName,
         String fileType
