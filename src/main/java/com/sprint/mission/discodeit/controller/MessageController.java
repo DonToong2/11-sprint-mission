0dd72dddaee98a50742c4b3f520b057fd566b59b
김명근 <rlaaudrms369@gmail.com>
2026-03-24 09:06:03 +0900

    Controller -> controller로 패키지명 변경

--- src/main/java/com/sprint/mission/discodeit/controller/MessageController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/MessageController.java
@@ -1,4 +1,4 @@
-package com.sprint.mission.discodeit.Controller;
+package com.sprint.mission.discodeit.controller;
 
 import com.sprint.mission.discodeit.dto.MessageCreateDto;
 import com.sprint.mission.discodeit.dto.MessageUpdateDto;
