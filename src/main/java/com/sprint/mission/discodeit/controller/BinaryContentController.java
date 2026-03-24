5ddcb300dd7504f91b04881c88a5db8933107427
김명근 <rlaaudrms369@gmail.com>
2026-03-24 09:19:45 +0900

    바이너리 파일(유저 프로필이미지, 메시지 첨부파일)을 관리(1개 이상 조회)하는 BinaryContentController 구현

---
+++ src/main/java/com/sprint/mission/discodeit/controller/BinaryContentController.java
@@ -0,0 +1,26 @@
+package com.sprint.mission.discodeit.controller;
+
+import com.sprint.mission.discodeit.entity.BinaryContent;
+import com.sprint.mission.discodeit.service.BinaryContentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController // Json 반환을 위해 @Controller 대신 @ResponseBody를 포함한 @RestController 사용
+@RequestMapping("/binary-content")
+@RequiredArgsConstructor
+public class BinaryContentController {
+    private final BinaryContentService binaryContentService;
+
+    // 바이너리 파일을 1개 또는 여러 개 조회(/binary-content?ids=...&ids=...)
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<List<BinaryContent>> readAll(@RequestParam List<UUID> ids) {
+        return ResponseEntity.ok(binaryContentService.findAllByIdIn(ids));
+    }
+}
