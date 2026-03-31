865af9faef2d2ea472c4bed5e2f9e9aab076c491
김명근 <rlaaudrms369@gmail.com>
2026-03-31 16:32:03 +0900

    swagger-ui - @Operation, @ApiResponse, @Parameter 애노테이션으로 BinaryContentController 문서화

--- src/main/java/com/sprint/mission/discodeit/controller/BinaryContentController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/BinaryContentController.java
@@ -3,6 +3,10 @@ package com.sprint.mission.discodeit.controller;
 import com.sprint.mission.discodeit.constant.EndPoints;
 import com.sprint.mission.discodeit.entity.BinaryContent;
 import com.sprint.mission.discodeit.service.BinaryContentService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
 import java.util.UUID;
@@ -26,17 +30,26 @@ public class BinaryContentController {
 
   // 여러 첨부파일 조회
   @ResponseStatus(HttpStatus.OK)
+  @Operation(summary = "여러 첨부 파일 조회")
+  @ApiResponse(responseCode = "200", description = "첨부 파일 목록 조회 성공")
   @RequestMapping(method = RequestMethod.GET)
   public ResponseEntity<List<BinaryContent>> findAllByIdIn(
+      @Parameter(description = "조회할 첨부 파일 ID 목록")
       @RequestParam List<UUID> binaryContentIds) {
     return ResponseEntity.ok(binaryContentService.findAllByIdIn(binaryContentIds));
   }
 
   // Id로 바이너리 파일을 단건 조회
   @ResponseStatus(HttpStatus.OK)
+  @Operation(summary = "첨부 파일 조회")
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "200", description = "첨부 파일 조회 성공"),
+      @ApiResponse(responseCode = "400", description = "첨부 파일을 찾을 수 없음")
+  })
   @RequestMapping(value = "/{binaryContentId}", method = RequestMethod.GET)
   public ResponseEntity<BinaryContent> find(
-      @PathVariable("binaryContentId") UUID id) {
+      @Parameter(description = "조회할 첨부 파일 ID")
+      @PathVariable(value = "binaryContentId") UUID id) {
     return ResponseEntity.ok(binaryContentService.find(id));
   }
 }
