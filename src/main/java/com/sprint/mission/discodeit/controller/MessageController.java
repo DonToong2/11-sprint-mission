265e95182f3b8d85612b6030e0258c75a9b277be
김명근 <rlaaudrms369@gmail.com>
2026-03-31 17:28:58 +0900

    swagger-ui - @Operation, @ApiResponse, @Parameter 애노테이션으로 MessageController 문서화

--- src/main/java/com/sprint/mission/discodeit/controller/MessageController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/MessageController.java
@@ -5,11 +5,16 @@ import com.sprint.mission.discodeit.dto.MessageCreateRequest;
 import com.sprint.mission.discodeit.dto.MessageUpdateRequest;
 import com.sprint.mission.discodeit.entity.Message;
 import com.sprint.mission.discodeit.service.MessageService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -31,33 +36,57 @@ public class MessageController {
 
   // 메시지 보내기(생성)
   @ResponseStatus(HttpStatus.CREATED)
-  @RequestMapping(method = RequestMethod.POST)
+  @Operation(summary = "Message 생성")
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "201", description = "Message가 성공적으로 생성됨"),
+      @ApiResponse(responseCode = "404", description = "Channel 또는 User를 찾을 수 없음")
+  })
+  @RequestMapping(method = RequestMethod.POST, consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
   public ResponseEntity<Message> create(
+      @Parameter(description = "Message 생성 정보")
       @RequestPart("messageCreateRequest") MessageCreateRequest dto,
+      @Parameter(description = "Message 첨부 파일들")
       @RequestPart(value = "attachments", required = false) List<MultipartFile> attachments) {
     return ResponseEntity.status(HttpStatus.CREATED).body(messageService.create(dto, attachments));
   }
 
   // 메시지 수정
   @ResponseStatus(HttpStatus.OK)
+  @Operation(summary = "Message 내용 수정")
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "200", description = "Message가 성공적으로 수행됨"),
+      @ApiResponse(responseCode = "404", description = "Message를 찾을 수 없음")
+  })
   @RequestMapping(value = "/{messageId}", method = RequestMethod.PATCH)
-  public ResponseEntity<Message> update(@PathVariable("messageId") UUID id,
+  public ResponseEntity<Message> update(
+      @Parameter(description = "수정할 Message ID")
+      @PathVariable("messageId") UUID id,
       @RequestBody MessageUpdateRequest dto) {
     return ResponseEntity.ok(messageService.update(id, dto));
   }
 
   // 메시지 삭제
   @ResponseStatus(HttpStatus.NO_CONTENT)
+  @Operation(summary = "Message 삭제")
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "204", description = "Message가 성공적으로 삭제됨"),
+      @ApiResponse(responseCode = "404", description = "Message를 찾을 수 없음")
+  })
   @RequestMapping(value = "/{messageId}", method = RequestMethod.DELETE)
-  public ResponseEntity<Void> delete(@PathVariable("messageId") UUID id) {
+  public ResponseEntity<Void> delete(
+      @Parameter(description = "삭제할 Message ID")
+      @PathVariable("messageId") UUID id) {
     messageService.delete(id);
     return ResponseEntity.noContent().build();
   }
 
   // 특정 채널의 모든 메시지를 조회
   @ResponseStatus(HttpStatus.OK)
+  @Operation(summary = "Channel의 Message 목록 조회")
+  @ApiResponse(responseCode = "200", description = "Message 목록 조회 성공")
   @RequestMapping(method = RequestMethod.GET)
   public ResponseEntity<List<Message>> readAllByChannelId(
+      @Parameter(description = "조회할 Channel ID")
       @RequestParam("channelId") UUID channelId) {
     return ResponseEntity.ok(messageService.findAllByChannelId(channelId));
   }
