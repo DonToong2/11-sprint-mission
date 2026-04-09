ecc12d090041ee346c7e8cc7924de7394628f69a
김명근 <rlaaudrms369@gmail.com>
2026-04-09 14:23:32 +0900

    refactor: BinaryContentService uses JpaRepository

--- src/main/java/com/sprint/mission/discodeit/service/basic/BasicBinaryContentService.java
+++ src/main/java/com/sprint/mission/discodeit/service/basic/BasicBinaryContentService.java
@@ -4,37 +4,43 @@ import com.sprint.mission.discodeit.dto.BinaryContentCreateRequest;
 import com.sprint.mission.discodeit.entity.BinaryContent;
 import com.sprint.mission.discodeit.repository.BinaryContentRepository;
 import com.sprint.mission.discodeit.service.BinaryContentService;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
-import java.util.UUID;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class BasicBinaryContentService implements BinaryContentService {
-    private final BinaryContentRepository binaryContentRepository;
 
-    @Override
-    public BinaryContent create(BinaryContentCreateRequest dto) {
-        BinaryContent binaryContent = dto.toBinaryContent();
-        binaryContentRepository.insert(binaryContent);
+  private final BinaryContentRepository binaryContentRepository;
 
-        return binaryContent;
-    }
+  @Override
+  @Transactional
+  public BinaryContent create(BinaryContentCreateRequest dto) {
+    BinaryContent binaryContent = dto.toBinaryContent();
+    binaryContentRepository.save(binaryContent);
 
-    @Override
-    public BinaryContent find(UUID id) {
-        return binaryContentRepository.findById(id);
-    }
+    return binaryContent;
+  }
 
-    @Override
-    public List<BinaryContent> findAllByIdIn(List<UUID> ids) {
-        return ids.stream().map(binaryContentRepository::findById).toList();
-    }
+  @Override
+  @Transactional(readOnly = true)
+  public Optional<BinaryContent> find(UUID id) {
+    return binaryContentRepository.findById(id);
+  }
 
-    @Override
-    public void delete(UUID id) {
-        binaryContentRepository.delete(id);
-    }
+  @Override
+  @Transactional(readOnly = true)
+  public List<BinaryContent> findAllByIdIn(List<UUID> ids) {
+    return binaryContentRepository.findAllById(ids);
+  }
+
+  @Override
+  @Transactional
+  public void delete(UUID id) {
+    binaryContentRepository.deleteById(id);
+  }
 }
