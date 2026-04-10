f2de0e5b27b21ce15e6b2892c762e113655d5647
김명근 <rlaaudrms369@gmail.com>
2026-04-10 14:51:21 +0900

    feat: Add ReadDto

--- src/main/java/com/sprint/mission/discodeit/dto/read/UserDto.java
+++ src/main/java/com/sprint/mission/discodeit/dto/read/UserDto.java
@@ -7,8 +7,8 @@ public record UserDto(
     UUID id,
     String username,
     String email,
-    UUID profileId, // 프로필 이미지
-    @JsonProperty("online") boolean isOnline // 유저의 상태(온라인이거나 오프라인), 심화 요구사항 적용(Enum -> boolean)
+    UUID profile, // 프로필 이미지
+    @JsonProperty("online") boolean online // 유저의 상태(온라인이거나 오프라인), 심화 요구사항 적용(Enum -> boolean)
     // User.Status isStatus // 유저의 상태(온라인이거나 오프라인)
 ) {
 
