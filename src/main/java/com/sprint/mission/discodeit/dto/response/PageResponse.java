5bf443cf588d451339208f95e09aeb8820dd2b7e
김명근 <rlaaudrms369@gmail.com>
2026-04-13 15:28:15 +0900

    feat: Add PageResponseMapper

--- src/main/java/com/sprint/mission/discodeit/dto/response/PageResponse.java
+++ src/main/java/com/sprint/mission/discodeit/dto/response/PageResponse.java
@@ -2,6 +2,7 @@ package com.sprint.mission.discodeit.dto.response;
 
 import java.util.List;
 
+// 페이징 처리 BinaryContentDto, UserDto 등 어떤 타입이 들어올지 모르기 때문에(= 어떤 타입이든 가능하도록)
 public record PageResponse<T>(
     List<T> content, // 실제 데이터
     int number, // 페이지 번호
--- src/main/java/com/sprint/mission/discodeit/repository/MessageRepository.java
+++ src/main/java/com/sprint/mission/discodeit/repository/MessageRepository.java
@@ -3,9 +3,19 @@ package com.sprint.mission.discodeit.repository;
 import com.sprint.mission.discodeit.entity.Message;
 import java.util.List;
 import java.util.UUID;
+import org.springdoc.core.converters.models.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MessageRepository extends JpaRepository<Message, UUID> {
+
+  Slice<Message> findByChannelIdOrderByCreatedAtDesc(UUID channelId, Pageable pageable);
+  // Pageable, 몇 번째 페이지(page)의 몇 개의 데이터(size)를 어떻게(sort, 이름순 최신순 등으로) 가져올지
+//    public Pageable(int page, int size, List<String> sort) {
+//    this.page = page;
+//    this.size = size;
+//    this.sort = sort;
+//  }
 
   List<Message> findAllByChannelId(UUID id);
 
