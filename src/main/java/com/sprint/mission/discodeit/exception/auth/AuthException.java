2f07207ac16a6a7b2f1dd85ad0dc8f7a7ca11965
김명근 <rlaaudrms369@gmail.com>
2026-04-30 18:57:16 +0900

    fix: detail 없이 ErrorCode만 받는 생성자도 추가

--- src/main/java/com/sprint/mission/discodeit/exception/auth/AuthException.java
+++ src/main/java/com/sprint/mission/discodeit/exception/auth/AuthException.java
@@ -6,6 +6,10 @@ import java.util.Map;
 
 public class AuthException extends DiscodeitException {
 
+  public AuthException(ErrorCode errorCode) {
+    super(errorCode);
+  }
+
   public AuthException(ErrorCode errorCode, Map<String, Object> details) {
     super(errorCode, details);
   }
