5c4ce6c54a537ecfdecd6c63d7b101087e31dbf4
김명근 <rlaaudrms369@gmail.com>
2026-05-06 15:36:53 +0900

    rename: 예외 클래스 이름 변경

--- src/main/java/com/sprint/mission/discodeit/exception/user/UserEmailAlreadyExistsException.java
+++ src/main/java/com/sprint/mission/discodeit/exception/user/UserEmailAlreadyExistsException.java
@@ -3,9 +3,9 @@ package com.sprint.mission.discodeit.exception.user;
 import com.sprint.mission.discodeit.exception.ErrorCode;
 import java.util.Map;
 
-public class UserEmailAlreadyExists extends UserException {
+public class UserEmailAlreadyExistsException extends UserException {
 
-  public UserEmailAlreadyExists(String email) {
+  public UserEmailAlreadyExistsException(String email) {
     super(ErrorCode.DUPLICATE_EMAIL, Map.of("email", email));
   }
 }
