5245762d537c53e1746f8e4c7b4313dd9126dee8
김명근 <rlaaudrms369@gmail.com>
2026-06-22 11:14:30 +0900

    refactor: Csrf Token 요청 핸들러 구현체를 커스텀 핸들러로 설정

--- src/main/java/com/sprint/mission/discodeit/config/SecurityConfig.java
+++ src/main/java/com/sprint/mission/discodeit/config/SecurityConfig.java
@@ -16,8 +16,11 @@ public class SecurityConfig {
             // CSRF Token Repository 구현체를 Cookie Csrf Token Repository로 설정(Default는 Http Session Csrf...)
             // js에 접근 가능하도록 HttpOnly 옵션을 false로 설정
             .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+            // CSRF Token 요청 핸들러 구현체를 커스텀 핸들러로 설정(Default는 XORCsrfTokenRequestAttributeHandler)
+            .csrfTokenRequestHandler(new SpaCsrfTokenRequestHandler())
         );
     return http.build();
   }
 
 }
+
