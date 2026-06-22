4ab247a267540f1fbc795efb0be0bacdf0f259cd
김명근 <rlaaudrms369@gmail.com>
2026-06-22 15:25:55 +0900

    refactor: 기존의 로그인 관련 로직 제거

--- src/main/java/com/sprint/mission/discodeit/controller/AuthController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/AuthController.java
@@ -2,34 +2,18 @@ package com.sprint.mission.discodeit.controller;
 
 import com.sprint.mission.discodeit.constant.EndPoints;
 import com.sprint.mission.discodeit.controller.api.AuthApi;
-import com.sprint.mission.discodeit.dto.request.LoginRequest;
-import com.sprint.mission.discodeit.dto.response.UserDto;
-import com.sprint.mission.discodeit.service.AuthService;
-import jakarta.validation.Valid;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController // Json 반환을 위해 @Controller 대신 @ResponseBody를 포함한 @RestController 사용
 @RequestMapping(EndPoints.AUTH)
-@RequiredArgsConstructor
 public class AuthController implements AuthApi {
-
-  private final AuthService authService;
-
-  @Override
-  @PostMapping("/login") // 보안을 위해 GET이 아닌 POST 사용
-  public ResponseEntity<UserDto> login(@Valid @RequestBody LoginRequest dto) {
-    return ResponseEntity.ok(authService.login(dto));
-  }
 
   @Override
   @GetMapping("/csrf-token")
