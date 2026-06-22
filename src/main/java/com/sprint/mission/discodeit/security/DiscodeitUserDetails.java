18cc6e90e6080f0f65e96e1e59e5029d7ab3a97c
김명근 <rlaaudrms369@gmail.com>
2026-06-22 14:12:16 +0900

    refactor: UserDto를 받을 수 있도록 @Getter 추가

--- src/main/java/com/sprint/mission/discodeit/security/DiscodeitUserDetails.java
+++ src/main/java/com/sprint/mission/discodeit/security/DiscodeitUserDetails.java
@@ -4,11 +4,13 @@ package com.sprint.mission.discodeit.security;
 import com.sprint.mission.discodeit.dto.response.UserDto;
 import java.util.Collection;
 import java.util.List;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+@Getter
 @RequiredArgsConstructor
 public class DiscodeitUserDetails implements UserDetails {
 
