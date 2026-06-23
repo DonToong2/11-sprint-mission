685f01705828f43293784823fdc748b936bbe1eb
김명근 <rlaaudrms369@gmail.com>
2026-06-23 13:39:37 +0900

    refactor: 로깅 메시지 추가

--- src/main/java/com/sprint/mission/discodeit/service/basic/BasicUserService.java
+++ src/main/java/com/sprint/mission/discodeit/service/basic/BasicUserService.java
@@ -179,12 +179,15 @@ public class BasicUserService implements UserService {
   // Role Update
   @Transactional
   public UserDto updateRole(UserRoleUpdateRequest dto) {
+    log.debug("[USER_ROLE_UPTATE_START] 유저 권한 수정 시작 - 권한 수정할 유저 ID={}", dto.userId());
 
     User user = userRepository.findById(dto.userId()).orElseThrow(
         () -> new UserNotFoundException(dto.userId())
     );
 
     user.updateRole(dto.newRole());
+
+    log.info("[USER_ROLE_UPDATE_SUCCESS] 유저 권한 수정 완료 - 권한 수정한 유저 ID={}", dto.userId());
 
     return userMapper.toDto(user);
   }
