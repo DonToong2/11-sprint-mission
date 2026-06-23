9deff060a612eeb9d2e65d537ad8461f53ea9e44
김명근 <rlaaudrms369@gmail.com>
2026-06-23 13:55:50 +0900

    feat: 사용자 권한 수정 API 구현

--- src/main/java/com/sprint/mission/discodeit/controller/AuthController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/AuthController.java
@@ -2,21 +2,29 @@ package com.sprint.mission.discodeit.controller;
 
 import com.sprint.mission.discodeit.constant.EndPoints;
 import com.sprint.mission.discodeit.controller.api.AuthApi;
+import com.sprint.mission.discodeit.dto.request.UserRoleUpdateRequest;
 import com.sprint.mission.discodeit.dto.response.UserDto;
 import com.sprint.mission.discodeit.security.DiscodeitUserDetails;
+import com.sprint.mission.discodeit.service.AuthService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController // Json 반환을 위해 @Controller 대신 @ResponseBody를 포함한 @RestController 사용
 @RequestMapping(EndPoints.AUTH)
+@RequiredArgsConstructor
 public class AuthController implements AuthApi {
+
+  private final AuthService authService;
 
   @Override
   @GetMapping("/csrf-token")
@@ -33,5 +41,15 @@ public class AuthController implements AuthApi {
       @AuthenticationPrincipal DiscodeitUserDetails userDetails
   ) {
     return ResponseEntity.status(HttpStatus.OK).body(userDetails.getUserDto());
+  }
+
+  @Override
+  @PutMapping("/role")
+  public ResponseEntity<UserDto> updateRole(
+      @RequestBody UserRoleUpdateRequest dto
+  ) {
+    UserDto user = authService.updateRole(dto);
+
+    return ResponseEntity.status(HttpStatus.OK).body(user);
   }
 }
--- src/main/java/com/sprint/mission/discodeit/controller/api/AuthApi.java
+++ src/main/java/com/sprint/mission/discodeit/controller/api/AuthApi.java
@@ -1,5 +1,6 @@
 package com.sprint.mission.discodeit.controller.api;
 
+import com.sprint.mission.discodeit.dto.request.UserRoleUpdateRequest;
 import com.sprint.mission.discodeit.dto.response.UserDto;
 import com.sprint.mission.discodeit.security.DiscodeitUserDetails;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -7,6 +8,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Tag(name = "Auth", description = "인증 API")
 public interface AuthApi {
@@ -17,5 +20,10 @@ public interface AuthApi {
   @GetMapping("/me")
   ResponseEntity<UserDto> me(
       @AuthenticationPrincipal DiscodeitUserDetails userDetails
+  );
+
+  @PutMapping("/role")
+  ResponseEntity<UserDto> updateRole(
+      @RequestBody UserRoleUpdateRequest dto
   );
 }
