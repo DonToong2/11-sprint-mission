ff5cfa8c82a6f3641d72c008817221e7b704736a
김명근 <rlaaudrms369@gmail.com>
2026-06-23 18:12:59 +0900

    feat: Public 채널 생성/수정/삭제는 'CHANNEL_MANAGER', 사용자 권한 수정은 'ADMIN' 권한만 가능하도록 수정

--- src/main/java/com/sprint/mission/discodeit/service/basic/BasicAuthService.java
+++ src/main/java/com/sprint/mission/discodeit/service/basic/BasicAuthService.java
@@ -9,6 +9,7 @@ import com.sprint.mission.discodeit.repository.UserRepository;
 import com.sprint.mission.discodeit.service.AuthService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -23,6 +24,7 @@ public class BasicAuthService implements AuthService {
   // Role Update
   @Override
   @Transactional
+  @PreAuthorize("hasRole('ADMIN')")
   public UserDto updateRole(UserRoleUpdateRequest dto) {
     log.debug("[USER_ROLE_UPTATE_START] 유저 권한 수정 시작 - 권한 수정할 유저 ID={}", dto.userId());
 
