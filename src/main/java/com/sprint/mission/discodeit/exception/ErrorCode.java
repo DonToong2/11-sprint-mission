258035fc776ab6cd3acb8900e1b15385fd20b22a
김명근 <rlaaudrms369@gmail.com>
2026-06-23 18:39:17 +0900

    feat: Json 응답용 에러코드 생성

--- src/main/java/com/sprint/mission/discodeit/exception/ErrorCode.java
+++ src/main/java/com/sprint/mission/discodeit/exception/ErrorCode.java
@@ -31,6 +31,8 @@ public enum ErrorCode {
 
   // 공통
   INVALID_REQUEST("잘못된 요청입니다."),
+  UNAUTHORIZED("인증이 필요합니다."),
+  FORBIDDEN("접근 권한이 없습니다"),
   INTERNAL_SERVER_ERROR("서버 내부 오류가 발생했습니다.");
 
   private final String message;
