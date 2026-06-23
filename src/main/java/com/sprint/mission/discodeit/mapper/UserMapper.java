9b0f8f640b40cfd3c30b25dfd8a7731018c0c22e
김명근 <rlaaudrms369@gmail.com>
2026-06-23 14:21:32 +0900

    fix: UserMapper.toDto에 role 매핑 추가

--- src/main/java/com/sprint/mission/discodeit/mapper/UserMapper.java
+++ src/main/java/com/sprint/mission/discodeit/mapper/UserMapper.java
@@ -20,8 +20,9 @@ public class UserMapper {
         user.getProfile() != null ? // profile(BinaryContentDto)
             binaryContentMapper.toDto(user.getProfile()) : null, // 프로필 이미지가 있을수도 없을수도 있음
         user.getStatus() != null // userStatus
-            && user.getStatus().status() == Status.ONLINE
+            && user.getStatus().status() == Status.ONLINE,
         // UserStatus가 존재하고 Status.ONLINE를 가지면 프론트엔드상에서 true(온라인)를, 그렇지 않으면 false(오프라인)를 반환
+        user.getRole()
     );
   }
 
