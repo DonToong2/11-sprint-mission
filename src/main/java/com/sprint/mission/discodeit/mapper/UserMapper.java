d1a1a842f9420d3dd1b45dc0a08950a2327e13b2
김명근 <rlaaudrms369@gmail.com>
2026-06-24 14:04:14 +0900

    refactor: UserStatus 대신 SessionRegistry를 활용하여 사용자의 로그인 여부를 판단하도록 변경

--- src/main/java/com/sprint/mission/discodeit/mapper/UserMapper.java
+++ src/main/java/com/sprint/mission/discodeit/mapper/UserMapper.java
@@ -2,8 +2,10 @@ package com.sprint.mission.discodeit.mapper;
 
 import com.sprint.mission.discodeit.dto.response.UserDto;
 import com.sprint.mission.discodeit.entity.User;
-import com.sprint.mission.discodeit.entity.User.Status;
+import com.sprint.mission.discodeit.security.DiscodeitUserDetails;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.session.SessionRegistry;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -12,6 +14,8 @@ public class UserMapper {
 
   private final BinaryContentMapper binaryContentMapper;
 
+  private final SessionRegistry sessionRegistry;
+
   public UserDto toDto(User user) {
     return new UserDto(
         user.getId(), // id
@@ -19,11 +23,21 @@ public class UserMapper {
         user.getEmail(), // email
         user.getProfile() != null ? // profile(BinaryContentDto)
             binaryContentMapper.toDto(user.getProfile()) : null, // 프로필 이미지가 있을수도 없을수도 있음
-        user.getStatus() != null // userStatus
-            && user.getStatus().status() == Status.ONLINE,
+        isOnline(user.getId()),
         // UserStatus가 존재하고 Status.ONLINE를 가지면 프론트엔드상에서 true(온라인)를, 그렇지 않으면 false(오프라인)를 반환
         user.getRole()
     );
+  }
+
+  public boolean isOnline(UUID userId) {
+
+    return sessionRegistry.getAllPrincipals().stream()
+        .filter(principal -> principal instanceof DiscodeitUserDetails)
+        .map(principal -> (DiscodeitUserDetails) principal)
+        .anyMatch(userDetails ->
+            userDetails.getUserDto().id().equals(userId)
+                && !sessionRegistry.getAllSessions(userDetails, false).isEmpty()
+        );
   }
 
 }
