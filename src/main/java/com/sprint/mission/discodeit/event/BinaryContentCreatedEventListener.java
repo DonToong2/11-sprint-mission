9de21bff125df13249b234ab52c71d55785becc8
김명근 <rlaaudrms369@gmail.com>
2026-07-07 17:47:00 +0900

    feat: BinaryContent 저장 성공 시 status를 SUCCESS, 실패 시 FAIL로 변경하도록 설정

--- src/main/java/com/sprint/mission/discodeit/event/BinaryContentCreatedEventListener.java
+++ src/main/java/com/sprint/mission/discodeit/event/BinaryContentCreatedEventListener.java
@@ -1,5 +1,7 @@
 package com.sprint.mission.discodeit.event;
 
+import com.sprint.mission.discodeit.entity.BinaryContent.BinaryContentStatus;
+import com.sprint.mission.discodeit.service.BinaryContentService;
 import com.sprint.mission.discodeit.storage.BinaryContentStorage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -11,12 +13,22 @@ import org.springframework.transaction.event.TransactionalEventListener;
 public class BinaryContentCreatedEventListener {
 
   private final BinaryContentStorage binaryContentStorage;
+  private final BinaryContentService binaryContentService;
 
   // 커밋 이후에 put 메서드가 실행되도록 설정
   // phase 생략 시 기본값 TransactionPhase.AFTER_COMMIT
   @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
   public void handle(BinaryContentCreatedEvent event) {
-    binaryContentStorage.put(event.binaryContentId(), event.bytes());
+
+    try {
+      binaryContentStorage.put(event.binaryContentId(), event.bytes());
+
+      // 성공 시 BinaryContent Status를 성공 상태로 변경
+      binaryContentService.updateStatus(event.binaryContentId(), BinaryContentStatus.SUCCESS);
+    } catch (Exception e) {
+      // 실패 시 BinaryContent Status를 실패 상태로 변경
+      binaryContentService.updateStatus(event.binaryContentId(), BinaryContentStatus.FAIL);
+    }
   }
 
 }
