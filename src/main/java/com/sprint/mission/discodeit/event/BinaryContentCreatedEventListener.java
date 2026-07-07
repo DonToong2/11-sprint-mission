f0d8b10f32ca4259e5a589eb45406314576a9c07
김명근 <rlaaudrms369@gmail.com>
2026-07-07 16:38:31 +0900

    feat: BinaryContentCreatedEvent 발행 시 바이너리 저장을 처리하는 이벤트 리스너(BinaryContentCreatedEventListener) 구현

---
+++ src/main/java/com/sprint/mission/discodeit/event/BinaryContentCreatedEventListener.java
@@ -0,0 +1,22 @@
+package com.sprint.mission.discodeit.event;
+
+import com.sprint.mission.discodeit.storage.BinaryContentStorage;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionPhase;
+import org.springframework.transaction.event.TransactionalEventListener;
+
+@Component
+@RequiredArgsConstructor
+public class BinaryContentCreatedEventListener {
+
+  private final BinaryContentStorage binaryContentStorage;
+
+  // 커밋 이후에 put 메서드가 실행되도록 설정
+  // phase 생략 시 기본값 TransactionPhase.AFTER_COMMIT
+  @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+  public void handle(BinaryContentCreatedEvent event) {
+    binaryContentStorage.put(event.id(), event.bytes());
+  }
+
+}
