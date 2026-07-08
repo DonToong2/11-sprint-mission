2a209b66ea728c40b662637904d2a3449d92cf24
김명근 <rlaaudrms369@gmail.com>
2026-07-08 09:59:07 +0900

    feat: Update Request DTO에 ReadStatus 알림 여부 수정에 필요한 newNotificationEnabled 필드 추가

--- src/main/java/com/sprint/mission/discodeit/dto/request/ReadStatusUpdateRequest.java
+++ src/main/java/com/sprint/mission/discodeit/dto/request/ReadStatusUpdateRequest.java
@@ -5,7 +5,11 @@ import java.time.Instant;
 
 public record ReadStatusUpdateRequest(
     @NotNull(message = "마지막으로 읽은 시간은 필수입니다.")
-    Instant newLastReadAt
+    Instant newLastReadAt,
+
+// boolean이 원시타입이기 때문에 null이 될 수 없음
+//    @NotNull(message = "알림 여부는 필수입니다.")
+    boolean newNotificationEnabled
 ) {
 
 }
