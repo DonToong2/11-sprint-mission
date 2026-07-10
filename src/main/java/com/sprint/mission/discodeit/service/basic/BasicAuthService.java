ede816b542213a3d2ed046b64eccc4114e78adb5
김명근 <rlaaudrms369@gmail.com>
2026-07-10 15:39:32 +0900

    refactor: Service와 Controller 책임 분리

--- src/main/java/com/sprint/mission/discodeit/service/basic/BasicAuthService.java
+++ src/main/java/com/sprint/mission/discodeit/service/basic/BasicAuthService.java
@@ -1,7 +1,5 @@
 package com.sprint.mission.discodeit.service.basic;
 
-import static com.sprint.mission.discodeit.security.jwt.provider.JwtTokenProvider.REFRESH_TOKEN_COOKIE_NAME;
-
 import com.sprint.mission.discodeit.dto.request.UserRoleUpdateRequest;
 import com.sprint.mission.discodeit.dto.response.UserDto;
 import com.sprint.mission.discodeit.entity.User;
@@ -14,13 +12,12 @@ import com.sprint.mission.discodeit.repository.UserRepository;
 import com.sprint.mission.discodeit.security.auth.DiscodeitUserDetails;
 import com.sprint.mission.discodeit.security.auth.DiscodeitUserDetailsService;
 import com.sprint.mission.discodeit.security.jwt.dto.JwtDto;
+import com.sprint.mission.discodeit.security.jwt.dto.RefreshTokenResult;
 import com.sprint.mission.discodeit.security.jwt.model.JwtInformation;
 import com.sprint.mission.discodeit.security.jwt.properties.JwtProperties;
 import com.sprint.mission.discodeit.security.jwt.provider.JwtTokenProvider;
 import com.sprint.mission.discodeit.security.jwt.registry.JwtRegistry;
 import com.sprint.mission.discodeit.service.AuthService;
-import jakarta.servlet.http.Cookie;
-import jakarta.servlet.http.HttpServletResponse;
 import java.time.Instant;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
@@ -78,7 +75,12 @@ public class BasicAuthService implements AuthService {
   }
 
   @Override
-  public JwtDto refresh(String refreshToken, HttpServletResponse response) {
+  public RefreshTokenResult refresh(String refreshToken) {
+
+    // 서버에 RefreshToken이 존재하지 않을 경우 예외처리
+    if (!jwtRegistry.hasActiveJwtInformationByRefreshToken(refreshToken)) {
+      throw new RefreshTokenInvalidException();
+    }
 
     // 토큰에서 사용자 식별 정보(subject)를 추출 후 UUID로 변환
     UUID userId = UUID.fromString(jwtTokenProvider.getSubject(refreshToken));
@@ -89,11 +91,6 @@ public class BasicAuthService implements AuthService {
 
     // JwtDto에 담기 위해 UserDto로 추출
     UserDto userDto = userDetails.getUserDto();
-
-    // 서버에 RefreshToken이 존재하지 않을 경우 예외처리
-    if (!jwtRegistry.hasActiveJwtInformationByRefreshToken(refreshToken)) {
-      throw new RefreshTokenInvalidException();
-    }
 
     // Refresh Token으로 Access Token 발급
     String accessToken = jwtTokenProvider.reIssueAccessToken(refreshToken);
@@ -116,24 +113,8 @@ public class BasicAuthService implements AuthService {
     // 기존 Refresh Token을 활용하여 발급받은 Access Token, New Refresh Token를 JwtInformation으로 Registry에 저장(=교체)
     jwtRegistry.rotateJwtInformation(refreshToken, newJwtInformation);
 
-    // Refresh Token 쿠키 교체
-    Cookie cookie = new Cookie(REFRESH_TOKEN_COOKIE_NAME, newRefreshToken);
+    JwtDto jwtDto = new JwtDto(userDto, accessToken);
 
-    // JavaScript 접근 차단
-    cookie.setHttpOnly(true);
-
-    // HTTPS에서만 접근 가능하도록 설정
-    cookie.setSecure(true);
-
-    // 내 도메인 내의 모든 URI에 쿠키 적용
-    cookie.setPath("/");
-
-    // 쿠키 유효기간을 30일로 설정(setMaxAge()는 초 단위이기 때문에 60을 곱하여 초 단위로 변환)
-    cookie.setMaxAge(jwtProperties.getRefreshTokenExpiration() * 60);
-
-    // 응답에 쿠키 추가
-    response.addCookie(cookie);
-
-    return new JwtDto(userDto, accessToken);
+    return new RefreshTokenResult(jwtDto, newRefreshToken);
   }
 }
