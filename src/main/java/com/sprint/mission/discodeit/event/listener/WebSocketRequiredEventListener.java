9930413adea8a58aea74f876bf9182af5e5d4df2
김명근 <rlaaudrms369@gmail.com>
2026-07-22 19:33:24 +0900

    refactor: Payload를 변경 전 MessageCreatedEvent에서 MessageDto로 변경

--- src/main/java/com/sprint/mission/discodeit/event/listener/WebSocketRequiredEventListener.java
+++ src/main/java/com/sprint/mission/discodeit/event/listener/WebSocketRequiredEventListener.java
@@ -1,5 +1,6 @@
 package com.sprint.mission.discodeit.event.listener;
 
+import com.sprint.mission.discodeit.dto.response.MessageDto;
 import com.sprint.mission.discodeit.event.MessageCreatedEvent;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
@@ -14,9 +15,13 @@ public class WebSocketRequiredEventListener {
   private final SimpMessagingTemplate messagingTemplate;
 
   // MessageService.create 메서드 후 클라이언트로 메시지 전송(클라이언트 기준으로는 메시지 수신)
+  // WebSocket 전송은 순서가 중요하기 때문에 동기 처리(@Async를 사용하지 않음)
   @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
   public void handleMessage(MessageCreatedEvent event) {
+    
+    MessageDto message = event.getData();
+
     messagingTemplate.convertAndSend("/sub/channels." + event.getData().channelId() + ".messages",
-        event);
+        message);
   }
 }
