b145a6a04e9e12383551984a7e845ff9e2a8c5b0
김명근 <rlaaudrms369@gmail.com>
2026-07-23 10:09:04 +0900

    refactor: 하나의 SSE 이벤트 리스너로 통합을 위해 이벤트 리스너명 변경, 메서드명을 handle → on으로 변경

--- src/main/java/com/sprint/mission/discodeit/event/SseRequiredEventListener.java
+++ src/main/java/com/sprint/mission/discodeit/event/SseRequiredEventListener.java
@@ -1,5 +1,6 @@
 package com.sprint.mission.discodeit.event;
 
+import com.sprint.mission.discodeit.dto.response.NotificationDto;
 import com.sprint.mission.discodeit.sse.service.SseService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -9,16 +10,19 @@ import org.springframework.transaction.event.TransactionalEventListener;
 
 @Component
 @RequiredArgsConstructor
-public class SseNotificationEventListener {
+public class SseRequiredEventListener {
 
   private final SseService sseService;
 
   @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
-  public void handle(NotificationCreatedEvent event) {
+  public void on(NotificationCreatedEvent event) {
+
+    NotificationDto dto = event.getData();
+
     sseService.send(
-        List.of(event.receiverId()),
+        List.of(dto.receiverId()),
         "notifications.created",
-        event.notificationDto()
+        dto
     );
   }
 
