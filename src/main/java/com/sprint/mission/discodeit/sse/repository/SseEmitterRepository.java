24d8c97cf48c4c25a9bc1f5905bce4273aed3591
김명근 <rlaaudrms369@gmail.com>
2026-07-23 09:49:20 +0900

    refactor: SSE 연결 설정 리펙토링 - Service의 connect 메서드에서 ping() 호출하도록 변경 - Emitter Repository에서 저장 시 Race condition 방지를 위해 CopyOnWriteArrayList로 변경, delete 메서드에서 emitters가 있을때만 receiverId Key 삭제(다른 스레드에서 Value 수정 시 Key 삭제 안되도록 변경)

--- src/main/java/com/sprint/mission/discodeit/sse/repository/SseEmitterRepository.java
+++ src/main/java/com/sprint/mission/discodeit/sse/repository/SseEmitterRepository.java
@@ -1,10 +1,11 @@
 package com.sprint.mission.discodeit.sse.repository;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.CopyOnWriteArrayList;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
@@ -15,15 +16,17 @@ public class SseEmitterRepository {
 
   public void save(UUID receiverId, SseEmitter sseEmitter) {
     // data 내부에 receiverId가 없을때만 새 List를 만들어서 추가
-    data.computeIfAbsent(receiverId, key -> new ArrayList<>()).add(sseEmitter);
+    // Race condition 방지하기 위해 일반 ArrayList가 아닌 CopyOnWriteArrayList를 사용
+    // 쓰기 작업(생성/수정/삭제)가 일어날 때 새 배열에 작업 반영 포함하여 복사시킴(복사 후 기존 배열은 GC에서 삭제)
+    data.computeIfAbsent(receiverId, key -> new CopyOnWriteArrayList<>()).add(sseEmitter);
   }
 
   public List<SseEmitter> findAllByReceiverId(UUID receiverId) {
     // receiverId가 없으면 비어있는 ArrayList를 반환(NPE 방지)
-    return data.getOrDefault(receiverId, new ArrayList<>());
+    return data.getOrDefault(receiverId, List.of());
   }
 
-  public ConcurrentMap<UUID, List<SseEmitter>> findAll() {
+  public Map<UUID, List<SseEmitter>> findAll() {
     return data;
   }
 
@@ -37,7 +40,7 @@ public class SseEmitterRepository {
 
       // 더 이상 Value가 없을 경우 Key까지 같이 삭제
       if (emitters.isEmpty()) {
-        data.remove(receiverId);
+        data.remove(receiverId, emitters);
       }
     }
   }
--- src/main/java/com/sprint/mission/discodeit/sse/service/SseService.java
+++ src/main/java/com/sprint/mission/discodeit/sse/service/SseService.java
@@ -21,9 +21,6 @@ public class SseService {
   public SseEmitter connect(UUID receiverId, UUID lastEventId) {
     SseEmitter emitter = new SseEmitter(Long.MAX_VALUE);
 
-    // receiverId는 컨트롤러의 DiscodeitUserDetails 사용자 ID
-    emitterRepository.save(receiverId, emitter);
-
     // 입력, 반환 둘 다 없음(Runnable)
     emitter.onCompletion(
         () -> emitterRepository.delete(receiverId, emitter)
@@ -37,8 +34,11 @@ public class SseService {
         emitterRepository.delete(receiverId, emitter)
     );
 
+    // receiverId는 컨트롤러의 DiscodeitUserDetails 사용자 ID
+    emitterRepository.save(receiverId, emitter);
+
     // 연결 실패 시 즉시 삭제가 아닌 cleanup을 통해 제거
-//    ping(emitter);
+    ping(emitter);
 
     // SseMessage ID가 있으면 유실된 데이터들을 다시 전송(유실된 데이터 없으면 [].forEach로
     if (lastEventId != null) {
